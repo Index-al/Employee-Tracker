7a639b15c8396039e04baeeced6942697cd8aeb8
Indexal <148807519+Index-al@users.noreply.github.com>
2024-02-01 20:28:02 -0600

    Added functionalty for query functions

--- Develop/db/index.js
+++ Develop/db/index.js
@@ -5,8 +5,11 @@ const connection = require('./db/connection');
 const logo = require('asciiart-logo');
 const consoleTable = require('console.table');
 
+
+// options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+
 // Import the queries from the lib folder
-const { viewAllEmployees, addEmployee, UpdateEmployeeRole, viewAllRoles, addRole, viewAllDepartments, addDepartment, viewEmployeesByDepartment, viewEmployeesByManager, updateEmployeeManager, deleteEmployee, deleteRole, deleteDepartment, viewDepartmentBudget } = require('./lib/queries');
+const { viewAllDepartments, viewAllRoles, viewAllEmployees, addDepartment, addRole, addEmployee, UpdateEmployeeRole, deleteDepartment, deleteRole, deleteEmployee } = require('./lib/queries');
 
 // ASCII Art Logo
 console.log(logo({ name: 'Employee Manager' }).render());
@@ -19,69 +22,55 @@ function mainMenu() {
             name: 'action',
             message: 'What would you like to do?',
             choices: [
-                'View all employees',
-                'Add employee',
-                'Update employee role',
-                'Delete employee',
+                '+ View all employees',
+                '+ Add employee',
+                '+ Update employee role',
+                '+ Delete employee',
                 '---',
-                'View all roles',
-                'Add role',
-                'View all departments',
-                'Add department',
-                'View all employees by department',
-                'View all employees by manager',
-                'Update employee manager',
-                'Delete role',
-                'Delete department',
-                'View department budget',
-                'Exit'
+                '+ View all roles',
+                '+ Add role',
+                '+ Delete role',
+                '---',
+                '+ View all departments',
+                '+ Add department',
+                '+ Delete department',
+                '---',
+                '+ Exit'
             ]
         }
     ]).then((answers) => {
         switch (answers.action) {
-            case 'View all employees':
+            case '+ View all employees':
                 viewAllEmployees();
                 break;
-            case 'Add employee':
+            case '+ Add employee':
                 addEmployee();
                 break;
-            case 'Update employee role':
+            case '+ Update employee role':
                 UpdateEmployeeRole();
                 break;
-            case 'Delete employee':
+            case '+ Delete employee':
                 deleteEmployee();
                 break;
-            case 'View all roles':
+            case '+ View all roles':
                 viewAllRoles();
                 break;
-            case 'Add role':
+            case '+ Add role':
                 addRole();
                 break;
-            case 'View all departments':
-                viewAllDepartments();
-                break;
-            case 'Add department':
-                addDepartment();
-                break;
-            case 'View all employees by department':
-                viewEmployeesByDepartment();
-                break;
-            case 'View all employees by manager':
-                viewEmployeesByManager();
-                break;
-            case 'Update employee manager':
-                updateEmployeeManager();
-                break;
-            case 'Delete role':
+            case '+ Delete role':
                 deleteRole();
                 break;
-            case 'Delete department':
+            case '+ View all departments':
+                viewAllDepartments();
+                break;
+            case '+ Add department':
+                addDepartment();
+                break;
+            case '+ Delete department':
                 deleteDepartment();
                 break;
-            case 'View department budget':
-                viewDepartmentBudget();
-                break;
-            case 'Exit':
+            case '+ Exit':
                 connection.end();
                 break;
         }
